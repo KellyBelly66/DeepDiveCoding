d11108e2cc831d66890bf3d7b3978c6a168f48d7
Kelly <kcockrell@cnm.edu>
2018-08-12 09:37:19 -0600

    Initial

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        private int MyVar;
+
+        public int MyProperty
+        {
+            get { return MyVar; }
+            set { MyVar = value; }
+
+        }
+
+
+        public ICollection<Enrollment>Enrollments { get; set; }
+    }
+}
